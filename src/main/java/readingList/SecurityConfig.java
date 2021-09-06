5d746710760ccc23eb16d07afeef22cd0fc8d850
ClementDunstanSiaffu <clementsiaffu95@gmail.com>
2021-09-06 06:29:57 +0300

    adding userDetailsService annonymous function to security configure

--- src/main/java/readingList/SecurityConfig.java
+++ src/main/java/readingList/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Autowired
-    private ReaderRepository repository;
+    private ReaderRepository readerRepository;
     
     @Override
     protected void configure (HttpSecurity http) throws Exception{
@@ -32,6 +32,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure (AuthenticationManagerBuilder auth) throws Exception{
         
-    
+        auth.userDetailsService(new UserDetailsService(){
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+                return readerRepository.findOne(username);
+            }
+        });
     }
 }
